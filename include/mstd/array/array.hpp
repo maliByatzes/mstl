8ef38b864d3907393dd7fb712bb894e263a53760
Malibongwe <malib2027@gmail.com>
2024-05-16 00:14:30 +0200

    add: more element access functions in <array>

--- include/mstd/array/array.hpp
+++ include/mstd/array/array.hpp
@@ -4,42 +4,77 @@
 #include <stdexcept>
 #include <type_traits>
 
-namespace mstd {
+namespace mstd
+{
 
-template<class T, std::size_t N>
-struct array {
-  // assert if T is MoveConstructible and MoveAssignable
-  static_assert(std::is_move_constructible_v<T>, "Type T must be MoveConstructible");
-  static_assert(std::is_move_assignable_v<T>, "Type T must be MoveAssignable");
+  template<class T, std::size_t N>
+  struct array
+  {
+    // assert if T is MoveConstructible and MoveAssignable
+    static_assert(std::is_move_constructible_v<T>, "Type T must be MoveConstructible");
+    static_assert(std::is_move_assignable_v<T>, "Type T must be MoveAssignable");
 
-  // member types
-  using value_type = T;
-  using size_type = std::size_t;
-  using difference_type = std::ptrdiff_t;
-  using reference = value_type&;
-  using const_reference = const value_type&;
-  using pointer = value_type*;
-  using const_pointer = const value_type*;
-  // omit iterator aliases for now
+    // member types
+    using value_type = T;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    // omit iterator aliases for now
 
-  value_type elems[N]; // NOLINT
+    value_type elems[N];  // NOLINT
 
-  // element access
+    // element access
 
-  constexpr reference at(size_type pos) {
-    if (pos >= N) {
-      throw std::out_of_range("pos is out of range, pos >= size()");
+    constexpr reference at(size_type pos)
+    {
+      if (pos >= N)
+      {
+        throw std::out_of_range("pos is out of range, pos >= size()");
+      }
+      return elems[pos];
     }
-    return elems[pos];
-  }
 
-  constexpr const_reference at(size_type pos) const {
-    if (pos >= N) {
-      throw std::out_of_range("pos is out of range, pos >= size()");
+    constexpr const_reference at(size_type pos) const
+    {
+      if (pos >= N)
+      {
+        throw std::out_of_range("pos is out of range, pos >= size()");
+      }
+      return elems[pos];
     }
-    return elems[pos];
-  }
-};
 
-}
+    constexpr reference operator[](size_type pos)
+    {
+      return elems[pos];
+    }
 
+    constexpr const_reference operator[](size_type pos) const
+    {
+      return elems[pos];
+    }
+
+    constexpr reference front()
+    {
+      return elems[0];
+    }
+
+    constexpr const_reference front() const
+    {
+      return elems[0];
+    }
+
+    constexpr reference back()
+    {
+      return elems[N - 1];
+    }
+
+    constexpr const_reference back() const
+    {
+      return elems[N - 1];
+    }
+  };
+
+}  // namespace mstd
--- test/src/array/array_test.cpp
+++ test/src/array/array_test.cpp
@@ -1,14 +1,56 @@
 #include "mstd/array/array.hpp"
+
 #include <gtest/gtest.h>
-#include <array>
 
 TEST(ArrayTest, CheckAtFunc)
 {
-  mstd::array<int, 3> data = { 5, 2, 6 }; // NOLINT
+  mstd::array<int, 3> data = { 5, 2, 6 };  // NOLINT
   ASSERT_EQ(data.at(0), 5);
   EXPECT_THROW(data.at(4), std::out_of_range);
 }
 
+TEST(ArrayTest, CheckConstAtFunc)
+{
+  const mstd::array<double, 4> data = { 4.56, 1.32, 7.23, 4.0 };
+  ASSERT_EQ(data.at(1), 1.32);
+  EXPECT_THROW(data.at(5), std::out_of_range);
+}
+
+TEST(ArrayTest, CheckSubscriptFunc)
+{
+  mstd::array<std::string, 3> data = { "Hello", "World", "!" };
+  ASSERT_EQ(data[2], "!");
+}
+
+TEST(ArrayTest, CheckConstSubscriptFunc)
+{
+  const mstd::array<std::string, 3> data = { "Hello", "World", "!" };
+  ASSERT_EQ(data[2], "!");
+}
+
+TEST(ArrayTest, CheckFrontFunc)
+{
+  mstd::array<int, 3> data = { 4, 2, 6 };  // NOLINT
+  ASSERT_EQ(data.front(), 4);
+}
+
+TEST(ArrayTest, CheckConstFrontFunc)
+{
+  const mstd::array<int, 3> data = { 4, 2, 6 };  // NOLINT
+  ASSERT_EQ(data.front(), 4);
+}
+
+TEST(ArrayTest, CheckBackFunc)
+{
+  mstd::array<char, 6> letters{ 'a', 'b', 'c', 'd', 'e', 'f' }; // NOLINT
+  ASSERT_EQ(letters.back(), 'f');
+}
+
+TEST(ArrayTest, CheckConstBackFunc)
+{
+  const mstd::array<char, 6> letters{ 'a', 'b', 'c', 'd', 'e', 'f' };
+  ASSERT_EQ(letters.back(), 'f');
+}
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
